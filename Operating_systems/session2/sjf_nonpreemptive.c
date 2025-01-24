ea5fc7e1170a2ee478b7fe37ff5ad10ae0495a2b
amateurmonke <86455454+amateurmonke@users.noreply.github.com>
2025-01-24 12:13:44 +0530

    Handson Session 2

---
+++ Operating_systems/session2/sjf_nonpreemptive.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void sortByBurstTime(int processes[], int bt[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (bt[j] > bt[j + 1]) {
+                
+                int temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                int temp_proc = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp_proc;
+            }
+        }
+    }
+}
+
+void findwt(int processes[], int wt[], int bt[], int n) {
+    wt[0] = 0;  
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+}
+
+
+void findtat(int processes[], int wt[], int bt[], int n, int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+
+void findavgTime(int processes[], int n, int bt[]) 
+{
+    sortByBurstTime(processes, bt, n);
+
+    int wt[n], tat[n], tot_wt = 0, tot_tat = 0;
+
+    findwt(processes, wt, bt, n);
+    findtat(processes, wt, bt, n, tat);
+
+    printf("Processes\tBurstTime\tWaitTime\tTurnaroundTime\n");
+    for (int i = 0; i < n; i++) {
+        tot_wt += wt[i];
+        tot_tat += tat[i];
+        printf("%d\t\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
+    }
+
+    float avg_wt = (float)tot_wt / n;
+    float avg_tat = (float)tot_tat / n;
+    printf("Avg. WT = %.2f\n", avg_wt);
+    printf("Avg. TAT = %.2f\n", avg_tat);
+}
+
+int main() {
+    int processes[] = {1, 2, 3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+    int burst_time[] = {10, 5, 8};  
+
+    findavgTime(processes, n, burst_time);
+
+    return 0;
+}
